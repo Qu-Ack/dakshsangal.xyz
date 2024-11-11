c1e70241ac79f96768750d15f2e7c722cafc4ca7
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-11-11 18:47:35 +0530

    blog editor has progressed

--- static/js/writer.js
+++ static/js/writer.js
@@ -1,13 +1,72 @@
-document.addEventListener("selectionchange", (e) => {
-  const selection = window.getSelection();
-  if (selection.anchorNode.data) {
-    const result = selection.anchorNode.data.substring(
-      selection.anchorOffset,
-      selection.focusOffset,
-    );
+const boldButton = document.querySelector(".bold-button");
+const italicsButton = document.querySelector(".italics-button");
+const codeButton = document.querySelector(".code-button");
+const imageButton = document.querySelector(".image-button");
 
-    if (result !== "") {
-      console.log(result);
+let divSelectedOrNot = false;
+
+InitializeEventListeners();
+
+document.addEventListener("selectionchange", (e) => {
+  const selection = getSelection();
+  if (selection.type == "Range" && selection.rangeCount > 0) {
+    if (selection.anchorNode.attributes) {
+      divSelectedOrNot = true;
+    } else {
+      divSelectedOrNot = false;
     }
   }
 });
+
+function divSelected(selectionObject, node, att) {
+  switch (att) {
+    case "BOLD":
+      node.textContent = node.textContent
+        .trim()
+        .replace(node.textContent.trim(), `**${node.textContent.trim()}**`);
+      break;
+    case "ITALICS":
+      node.textContent = node.textContent
+        .trim()
+        .replace(node.textContent.trim(), `__${node.textContent.trim()}__`);
+      break;
+  }
+}
+
+function textSelected(selectionObject, node, att) {
+  console.log("in textSelected");
+  const text = node.parentNode.textContent;
+  const word = text
+    .substring(selectionObject.anchorOffset, selectionObject.focusOffset)
+    .trim();
+  switch (att) {
+    case "BOLD":
+      node.parentNode.textContent = text.replace(word, `**${word}**`);
+      break;
+    case "ITALICS":
+      node.parentNode.textContent = text.replace(word, `__${word}__`);
+      break;
+  }
+}
+
+function InitializeEventListeners() {
+  boldButton.addEventListener("click", (e) => {
+    const selection = document.getSelection();
+    if (divSelectedOrNot) {
+      divSelected(selection, selection.anchorNode, "BOLD");
+      return;
+    } else {
+      textSelected(selection, selection.anchorNode, "BOLD");
+    }
+  });
+
+  italicsButton.addEventListener("click", (e) => {
+    const selection = document.getSelection();
+    if (divSelectedOrNot) {
+      divSelected(selection, selection.anchorNode, "ITALICS");
+      return;
+    } else {
+      textSelected(selection, selection.anchorNode, "ITALICS");
+    }
+  });
+}

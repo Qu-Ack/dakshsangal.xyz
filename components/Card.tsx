ac5bea54815d30d62cabf98dd3eb2705a6649c28
quack <quack@quack.quack>
2025-04-05 23:07:51 +0530

    daksdhg

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ export default function Card(props: {
 			<img
 				src={props.url}
 				alt={props.alt}
-				className="h-150 max-w-2xl object-cover rounded-lg shadow-md"
+				className="h-150 max-w-4xl object-cover rounded-lg shadow-md"
 			/>
 			<div className="italic text-center text-sm">{props.quote}</div>
 		</div>

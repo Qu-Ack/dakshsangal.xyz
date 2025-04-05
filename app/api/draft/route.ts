09cb4b7a5932b5125f7be231bb934930f0266126
quack <quack@quack.quack>
2025-04-05 16:03:15 +0530

    cors is fucking stupid

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -13,7 +13,7 @@ const getCorsHeaders = (origin: string) => {
 	const headers = {
 		"Access-Control-Allow-Methods": `${process.env.ALLOWED_METHODS}`,
 		"Access-Control-Allow-Headers": `${process.env.ALLOWED_HEADERS}`,
-		"Access-Control-Allow-Origin": `${process.env.DOMAIN_URL}`,
+		"Access-Control-Allow-Origin": `${process.env.ALLOWED_ORIGIN}`,
 	};
 
 	// If no allowed origin is set to default server origin

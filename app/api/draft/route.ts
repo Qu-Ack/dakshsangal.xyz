a4456a402c5b564cfcc0664825f95e64ed2e8d06
quack <quack@quack.quack>
2025-04-05 16:31:55 +0530

    cors logging

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -9,20 +9,16 @@ const createBlogSchema = z.object({
 });
 
 const getCorsHeaders = (origin: string) => {
-	// Default options
 	const headers = {
 		"Access-Control-Allow-Methods": `${process.env.ALLOWED_METHODS}`,
 		"Access-Control-Allow-Headers": `${process.env.ALLOWED_HEADERS}`,
 		"Access-Control-Allow-Origin": `${process.env.ALLOWED_ORIGIN}`,
 	};
 
-	// If no allowed origin is set to default server origin
 	if (!process.env.ALLOWED_ORIGIN || !origin) return headers;
 
-	// If allowed origin is set, check if origin is in allowed origins
 	const allowedOrigins = process.env.ALLOWED_ORIGIN.split(",");
 
-	// Validate server origin
 	if (allowedOrigins.includes("*")) {
 		headers["Access-Control-Allow-Origin"] = "*";
 	} else if (allowedOrigins.includes(origin)) {
@@ -40,6 +36,7 @@ const getCorsHeaders = (origin: string) => {
  */
 export const OPTIONS = async (request: NextRequest) => {
 	// Return Response
+	console.log("options request in blogs");
 	return NextResponse.json(
 		{},
 		{

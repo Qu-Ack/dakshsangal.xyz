53e9407a9f09332d09aa1e20f8caf0f73f9fd059
quack <quack@quack.quack>
2025-04-05 18:05:39 +0530

    blogs page fix

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -17,7 +17,7 @@ function BlogsError({ error }: { error: Error }) {
 
 async function BlogsList() {
 	try {
-		const response = await fetch(`http://localhost:3000/api/blog`);
+		const response = await fetch(`/api/blog`);
 
 		if (!response.ok) {
 			console.log(response);
